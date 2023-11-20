de10f54539c202bf86830b08ff4d588cf6e4a95f
Ari SWS <ariastronout@gmail.com>
2023-11-20 22:48:32 +0700

    migrate application plugin to version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
+    alias(libs.plugins.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.com.android.library) apply false
     alias(libs.plugins.ksp) apply false
